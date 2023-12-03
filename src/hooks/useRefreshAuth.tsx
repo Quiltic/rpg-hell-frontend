7b99077e87bc4b88f0036bdbe5aa21cc96e4238d
ecfidler <ethancf1999@gmail.com>
2023-12-03 12:17:19 -0500

    bye old api

--- src/hooks/useRefreshAuth.tsx
+++ src/hooks/useRefreshAuth.tsx
@@ -1,12 +1,10 @@
-import axios from "../api/axios";
-
 const useRefreshToken = () => {
     const refresh = async () => {
         try {
-            const response = await axios.get("/refresh", {
-                withCredentials: true,
-            });
-            console.log(response.data);
+            // const response = await axios.get("/refresh", {
+            //     withCredentials: true,
+            // });
+            // console.log(response.data);
             return true; // Login Refreshed
         } catch (err) {
             return false; // Logged Out
