9ceee44cc9f0d942778d4eec8f1566ef59142edc
Quiltic <jbot237@gmail.com>
2023-12-06 10:55:08 -0500

    Markdown?

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import "./App.css";
+import style from './markdown-styles.module.css';
 
-import { Button } from "./components/ui/Button/Button";
+// import { Button } from "./components/ui/Button/Button";
+
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg) with ~strikethrough~ and a URL: https://reactjs.org.
+![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg)
+* Lists
+* [ ] t![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg)odo
+* [x] done![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg)
+
+
+A table:![FIRE]()
+
+
+| Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text ![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg)|
+
+![FIRE](https://i.pngimg.me/thumb/f/720/m2i8A0d3m2b1G6b1.jpg)
+
+`
 
 function App() {
     return (
         <>
             <div>
-                <h2 className="text-xl pb-2">Button Testing</h2>
-                <div className="mx-auto w-1/4 grid gap-4 grid-cols-3">
-                    <Button>Button</Button>
-                    <Button variant={"mind"}>mind</Button>
-                    <Button variant={"body"}>body</Button>
-                    <Button variant={"soul"}>soul</Button>
-                    <Button variant={"subtle-mind"}>mind</Button>
-                    <Button variant={"subtle-body"}>body</Button>
-                    <Button variant={"subtle-soul"}>body</Button>
-                    <Button variant={"link-mind"}>mind</Button>
-                    <Button variant={"link-body"}>body</Button>
-                    <Button variant={"link-soul"}>body</Button>
-                    <Button variant={"link"}>yup</Button>
-                    <Button variant={"subtle"}>yup</Button>
-                </div>
+            <Markdown remarkPlugins={[remarkGfm]} className={style.reactMarkDown}>{markdown}</Markdown>
             </div>
         </>
     );
 }
 
 export default App;
+
+
+
