1993a1136046ac9a3a2ed0dbe01e53a196e51a79
ecfidler <ethancf1999@gmail.com>
2023-12-06 20:14:41 -0500

    better buttons with icons yeah

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,24 @@ import Login from "./components/auth/Login";
 
 import { Button } from "./components/ui/Button/Button";
 
-import { BookOpenIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
+
+const bookOpenIcon = (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+        />
+    </svg>
+);
 
 function App() {
     return (
@@ -12,10 +28,11 @@ function App() {
             <div>
                 <Login />
 
-                <Button leftIcon={<BookOpenIcon />} variant="link-body">
-                    Rulebook / Josh land
-                </Button>
-                <Link to={"rulebook"}>Actual link button</Link>
+                <Link to={"rulebook"}>
+                    <Button leftIcon={bookOpenIcon} variant="body">
+                        Rulebook / Josh land
+                    </Button>
+                </Link>
             </div>
         </>
     );
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -6,7 +6,40 @@ import { Button } from "../ui/Button/Button";
 import { Popover, Transition } from "@headlessui/react";
 
 import discordLogo from "../../assets/discord.svg";
-import { ArrowLeftOnRectangleIcon, UserIcon } from "@heroicons/react/20/solid";
+
+const arrowLeftOnRectangleIcon = (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+        />
+    </svg>
+);
+
+const userIcon = (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+        />
+    </svg>
+);
 
 export default function Login() {
     const { auth, setAuth } = useContext(AuthContext);
@@ -59,14 +92,14 @@ export default function Login() {
                             <Popover.Panel className="relative z-10 mt-3 right-3 px-2 w-48 h-20 rounded-lg bg-light-100 dark:bg-dark-100">
                                 <div className="grid h-20 content-evenly grid-cols-1">
                                     <Button
-                                        leftIcon={<ArrowLeftOnRectangleIcon />}
+                                        leftIcon={arrowLeftOnRectangleIcon}
                                         variant="body"
                                         className="h-8 rounded-lg"
                                     >
                                         Profile
                                     </Button>
                                     <Button
-                                        leftIcon={<UserIcon />}
+                                        leftIcon={userIcon}
                                         variant="mind"
                                         className="h-8 rounded-lg"
                                     >
