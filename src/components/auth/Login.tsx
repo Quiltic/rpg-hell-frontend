67e43ffe9f0d7df715f825639dab354d8702345a
ecfidler <ethancf1999@gmail.com>
2023-12-06 19:30:32 -0500

    slightly better login button

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,45 @@
 import { useContext, useEffect, Fragment } from "react";
 import { AuthContext } from "../../context/AuthProvider";
 import { IAuthenticatedUser } from "../../types/auth";
-// import { Button } from "../ui/Button/Button";
+import { Button } from "../ui/Button/Button";
 
 import { Popover, Transition } from "@headlessui/react";
 
 import discordLogo from "../../assets/discord.svg";
+
+const arrowLeftOnRectangleIcon = (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+        />
+    </svg>
+);
+
+const userIcon = (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+        />
+    </svg>
+);
 
 export default function Login() {
     const { auth, setAuth } = useContext(AuthContext);
@@ -55,11 +89,22 @@ export default function Login() {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute z-10 mt-3 w-48 h-16 rounded-lg bg-light-100 dark:bg-dark-100">
-                                <div className="grid h-16 content-evenly grid-cols-1">
-                                    <div>Profile</div>
-                                    <div>dont judge I need to fix it</div>
-                                    <div>Log out</div>
+                            <Popover.Panel className="relative z-10 mt-3 right-3 px-2 w-48 h-20 rounded-lg bg-light-100 dark:bg-dark-100">
+                                <div className="grid h-20 content-evenly grid-cols-1">
+                                    <Button
+                                        leftIcon={arrowLeftOnRectangleIcon}
+                                        variant="body"
+                                        className="h-8 rounded-lg"
+                                    >
+                                        Profile
+                                    </Button>
+                                    <Button
+                                        leftIcon={userIcon}
+                                        variant="mind"
+                                        className="h-8 rounded-lg"
+                                    >
+                                        Log out
+                                    </Button>
                                 </div>
                             </Popover.Panel>
                         </Transition>
