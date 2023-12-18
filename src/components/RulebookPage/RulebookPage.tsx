85e594ef06e40c2d02c556268608fc88938de2a5
Quiltic <jbot237@gmail.com>
2023-12-18 17:25:45 -0500

    Tried to figure out Forms. It did not go well

--- src/components/RulebookPage/RulebookPage.tsx
+++ src/components/RulebookPage/RulebookPage.tsx
@@ -7,6 +7,22 @@ import jsonfile from "../../assets/temp_trait.json";
 
 import { highlightKeywords, convertDictionaryToMD_Traits } from "../../util/markdownTools";
 
+function formdata() {
+    const _name: HTMLInputElement = document.getElementById("TraitNameInput");
+    const _requirements: HTMLInputElement = document.getElementById("TraitReqInput");
+    const _dice: HTMLInputElement = document.getElementById("TraitDiceInput");
+    const _effect: HTMLInputElement = document.getElementById("TraitEffectInput");
+
+    console.log(_name,_requirements);
+    // const jsonitem = {
+    //     name: _name.value.trim(),
+    //     requirements: _requirements.value.trim(),
+    //     dice: _dice.value.trim(),
+    //     effect: _effect.value.trim(),
+
+    // }
+}
+
 
 
 // const md = highlightKeywords(markdown);
@@ -17,6 +33,18 @@ console.log(md);
 export default function RulebookPage() {
     return (
         <div>
+            {/* <form onSubmit={formdata()}>
+                <label>Name</label>
+                <input type="text" placeholder="Name" id="TraitNameInput"/>
+                <label>Requirements</label>
+                <input type="text" placeholder="Body" id="TraitReqInput"/>
+                <label>Dice</label>
+                <input type="text" placeholder="##" id="TraitDiceInput"/>
+                <label>Effect</label>
+                <input type="text" placeholder="wee" id="TraitEffectInput"/>
+                <input type="submit" value={"Submit"}/>
+            </form> */}
+
             <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
                 {md}
             </Markdown>
