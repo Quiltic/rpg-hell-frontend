20687a7d4fa527b334931bcad84cefc79b9a8925
ecfidler <ethancf1999@gmail.com>
2023-12-29 23:34:24 -0500

    adding fix to api

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -11,9 +11,10 @@ import {
 } from "../client";
 
 function useApi() {
-    // OpenAPI.BASE = "http://192.168.1.225/";
+    // OpenAPI.BASE = "http://192.168.1.225";
     OpenAPI.BASE = "http://localhost:8000";
     OpenAPI.WITH_CREDENTIALS = true;
+    OpenAPI.CREDENTIALS = "include";
 
     return {
         CreaturesService,
