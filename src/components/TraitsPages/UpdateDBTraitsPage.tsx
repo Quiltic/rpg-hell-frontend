74ffb68a44628d063788972d0e7c87ca3385a1e9
ecfidler <ethancf1999@gmail.com>
2024-01-31 17:03:24 -0500

    correct form

--- src/components/TraitsPages/UpdateDBTraitsPage.tsx
+++ src/components/TraitsPages/UpdateDBTraitsPage.tsx
@@ -11,9 +11,7 @@ import TraitsTable from "./TraitsTable";
 
 import json from "../../assets/OfflineJsons/Traits.json";
 import { Button } from "../ui/Button/Button";
-import {
-    sortArrayByReqs,
-} from "../../util/sortingTools";
+import { sortArrayByReqs } from "../../util/sortingTools";
 import { classNames, getPersistentPinnedNames } from "../../util/tableTools";
 
 import { ChevronIcon } from "../../assets/IconSVGs/heroiconsSVG";
@@ -24,79 +22,68 @@ function getTabWidth(lengthOfName: number) {
     return lengthOfName < 5 ? "w-12" : lengthOfName < 7 ? "w-16" : "w-20";
 }
 
-
-
-
 const statSkillList = [
-    'base 0',
-    'body 1',
-    'mind 1',
-    'soul 1',
-    'arcana 1',
-    'charm 1',
-    'crafting 1',
-    'medicine 1',
-    'nature 1',
-    'thieving 1',
-    'body 2',
-    'mind 2',
-    'soul 2',
-    'arcana 2',
-    'charm 2',
-    'crafting 2',
-    'medicine 2',
-    'nature 2',
-    'thieving 2',
-    'body 3',
-    'mind 3',
-    'soul 3',
-    'arcana 3',
-    'charm 3',
-    'crafting 3',
-    'medicine 3',
-    'nature 3',
-    'thieving 3',
-    'body 4',
-    'mind 4',
-    'soul 4',
-    'arcana 4',
-    'charm 4',
-    'crafting 4',
-    'medicine 4',
-    'nature 4',
-    'thieving 4',
-    'body 5',
-    'mind 5',
-    'soul 5',
-    'arcana 5',
-    'charm 5',
-    'crafting 5',
-    'medicine 5',
-    'nature 5',
-    'thieving 5',
-    'body 6',
-    'mind 6',
-    'soul 6',
-    'arcana 6',
-    'charm 6',
-    'crafting 6',
-    'medicine 6',
-    'nature 6',
-    'thieving 6',
-    'MONSTER 0'
+    "base 0",
+    "body 1",
+    "mind 1",
+    "soul 1",
+    "arcana 1",
+    "charm 1",
+    "crafting 1",
+    "medicine 1",
+    "nature 1",
+    "thieving 1",
+    "body 2",
+    "mind 2",
+    "soul 2",
+    "arcana 2",
+    "charm 2",
+    "crafting 2",
+    "medicine 2",
+    "nature 2",
+    "thieving 2",
+    "body 3",
+    "mind 3",
+    "soul 3",
+    "arcana 3",
+    "charm 3",
+    "crafting 3",
+    "medicine 3",
+    "nature 3",
+    "thieving 3",
+    "body 4",
+    "mind 4",
+    "soul 4",
+    "arcana 4",
+    "charm 4",
+    "crafting 4",
+    "medicine 4",
+    "nature 4",
+    "thieving 4",
+    "body 5",
+    "mind 5",
+    "soul 5",
+    "arcana 5",
+    "charm 5",
+    "crafting 5",
+    "medicine 5",
+    "nature 5",
+    "thieving 5",
+    "body 6",
+    "mind 6",
+    "soul 6",
+    "arcana 6",
+    "charm 6",
+    "crafting 6",
+    "medicine 6",
+    "nature 6",
+    "thieving 6",
+    "MONSTER 0",
 ];
 
-const otherListCore = [
-    'BROKEN 0',
-    'OOC 0'
-];
+const otherListCore = ["BROKEN 0", "OOC 0"];
 
-const diceCostListCore = [
-    'P',
-    '#',
-    '##',
-    '###'
-];
+const diceCostListCore = ["P", "#", "##", "###"];
 
 const IterativeTraitLevels = [
     "Body",
@@ -121,7 +108,8 @@ export default function UpdateDBTraitsPage() {
     const [searchValue, setSearchValue] = useState("");
     const [allTraits, setAllTraits] = useState<Array<Trait>>([]);
     const [displayedTraits, setDisplayedTraits] = useState<Array<Trait>>([]);
-    const [clearButtonVisibility, setClearButtonVisibility] = useState("hidden");
+    const [clearButtonVisibility, setClearButtonVisibility] =
+        useState("hidden");
 
     // const [mainstatSkillList, setMainstatSkillList] = useState(statSkillList);
     // const [secondstatSkillList, setSecondstatSkillList] = useState(statSkillList);
@@ -181,7 +169,6 @@ export default function UpdateDBTraitsPage() {
     }, [allTraits, searchValue]);
 
     function addToPinnedTrait(s: Trait) {
-
         setCurID(s.id);
         setNameText(s.name);
         setEffectText(s.effect ?? "");
@@ -204,67 +191,77 @@ export default function UpdateDBTraitsPage() {
     function removeFromPinnedTrait() {
         // Set inputs to nothing
         setCurID(0);
-        setNameText('');
-        setEffectText('');
-        setMainStat('');
-        setSecondStat('');
-        setDiceCost('');
-        setOtherDrop('');
-        setEffectText('');
+        setNameText("");
+        setEffectText("");
+        setMainStat("");
+        setSecondStat("");
+        setDiceCost("");
+        setOtherDrop("");
+        setEffectText("");
     }
 
-
-    
     async function handleCreateNew() {
         console.log(curTrait);
-        if (curTrait?.name != ""){
-            const reply = await TraitsService.putTrait(curTrait);
+        if (curTrait == undefined) {
+            return;
+        }
+        if (curTrait?.name != "") {
+            const reply = await TraitsService.putTrait({
+                requestBody: curTrait,
+            });
             setPopupData(reply);
-            setPopupName("Create New")
+            setPopupName("Create New");
             setPopupIsOpen(true);
             console.log(reply);
         }
         // Set inputs to nothing
         removeFromPinnedTrait();
-    };
-
+    }
 
     async function handleUpdate() {
         console.log(curTrait);
-        if (curTrait?.name != ""){
-            const reply = await TraitsService.updateTrait(curTrait?.name, curTrait);
+        if (curTrait == undefined) {
+            return;
+        }
+        if (curTrait?.name != "") {
+            const reply = await TraitsService.updateTrait({
+                name: curTrait?.name,
+                requestBody: curTrait,
+            });
             setPopupData(reply);
-            setPopupName("Update")
+            setPopupName("Update");
             setPopupIsOpen(true);
             console.log(reply);
         }
         // Set inputs to nothing
         removeFromPinnedTrait();
-    };
-
+    }
 
     async function handleDelete() {
         console.log(curTrait);
-        if (curTrait?.name != ""){
-            const reply = await TraitsService.deleteTrait(curTrait?.id);
+        if (curTrait?.id == undefined) {
+            return;
+        }
+        if (curTrait?.name != "") {
+            const reply = await TraitsService.deleteTrait({ id: curTrait?.id });
             setPopupData(reply);
-            setPopupName("Deleate")
+            setPopupName("Delete");
             setPopupIsOpen(true);
             console.log(reply);
         }
         // Set inputs to nothing
         removeFromPinnedTrait();
-    };
-    
+    }
+
     useEffect(() => {
         // console.log(mainStat,secondStat,otherDrop);
-        let trait = {
-            "id": curID,
-            "name": nameText.toLowerCase(),
-            "effect": effectText,
-            "req": [mainStat,secondStat,otherDrop],
-            "dice": 0,
-            "is_passive": true
+        const trait = {
+            id: curID,
+            name: nameText.toLowerCase(),
+            effect: effectText,
+            req: [mainStat, secondStat, otherDrop],
+            dice: 0,
+            is_passive: true,
         };
 
         if (diceCost != "P") {
@@ -273,77 +270,149 @@ export default function UpdateDBTraitsPage() {
         }
 
         // remove the empty stuffs
-        trait.req = trait.req.filter((str) => str !== '');
+        trait.req = trait.req.filter((str) => str !== "");
 
         setCurTrait(trait);
-
-    }, [nameText,diceCost,mainStat,secondStat,otherDrop,effectText]);
-
+    }, [
+        nameText,
+        diceCost,
+        mainStat,
+        secondStat,
+        otherDrop,
+        effectText,
+        curID,
+    ]);
 
     // Styling:
 
     return (
         <>
-        
-        <div className="grid grid-rows-auto-auto-auto-1fr-auto gap-4 p-4 bg-dark-400 rounded-md">
-            <div className="grid grid-cols-5 gap-4 bg-dark-300">
-                <div className="col-span-1">
-                    <div className="flex flex-row capitalize">Name</div>
-                    <input type="text" placeholder="Yoyo" className="flex flex-row" value={nameText}  onChange={(e) => setNameText(e.target.value)}/>
+            <div className="grid grid-rows-auto-auto-auto-1fr-auto gap-4 p-4 bg-dark-400 rounded-md">
+                <div className="grid grid-cols-5 gap-4 bg-dark-300">
+                    <div className="col-span-1">
+                        <div className="flex flex-row capitalize">Name</div>
+                        <input
+                            type="text"
+                            placeholder="Yoyo"
+                            className="flex flex-row"
+                            value={nameText}
+                            onChange={(e) => setNameText(e.target.value)}
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <div className="flex flex-row capitalize">
+                            Dice Cost
+                        </div>
+                        <CleanCombobox
+                            items={diceCostListCore}
+                            className="flex flex-row"
+                            selected={diceCost}
+                            setSelected={(val) => {
+                                setDiceCost(val);
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <div className="flex flex-row capitalize">
+                            Main Stat/Skill
+                        </div>
+                        <CleanCombobox
+                            items={statSkillList}
+                            className="flex flex-row"
+                            selected={mainStat}
+                            setSelected={(val) => {
+                                setMainStat(val);
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <div className="flex flex-row capitalize">
+                            Secondary Stat/Skill
+                        </div>
+                        <CleanCombobox
+                            items={statSkillList}
+                            className="flex flex-row"
+                            selected={secondStat}
+                            setSelected={(val) => {
+                                setSecondStat(val);
+                            }}
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <div className="flex flex-row capitalize">Other</div>
+                        <CleanCombobox
+                            items={otherListCore}
+                            className="flex flex-row"
+                            selected={otherDrop}
+                            setSelected={(val) => {
+                                setOtherDrop(val);
+                            }}
+                        />
+                    </div>
                 </div>
-                <div className="col-span-1">
-                    <div className="flex flex-row capitalize">Dice Cost</div>
-                    <CleanCombobox items={diceCostListCore} className="flex flex-row" selected={diceCost} setSelected={(val) => {setDiceCost(val);}}/>
-                </div>
-                <div className="col-span-1">
-                    <div className="flex flex-row capitalize">Main Stat/Skill</div>
-                    <CleanCombobox items={statSkillList} className="flex flex-row" selected={mainStat} setSelected={(val) => {setMainStat(val);}}/>
-                </div>
-                <div className="col-span-1">
-                    <div className="flex flex-row capitalize">Secondary Stat/Skill</div>
-                    <CleanCombobox items={statSkillList} className="flex flex-row" selected={secondStat} setSelected={(val) => {setSecondStat(val);}}/>
-                </div>
-                <div className="col-span-1">
-                    <div className="flex flex-row capitalize">Other</div>
-                    <CleanCombobox items={otherListCore} className="flex flex-row" selected={otherDrop} setSelected={(val) => {setOtherDrop(val);}}/>
-                </div>
-            </div>
 
-            <div className="">Effect</div>
-            <textarea rows={40} cols={50} placeholder="Whip around like a yoyo" className="bg-dark-300 h-44" value={effectText}  onChange={(e) => setEffectText(e.target.value)}/>
+                <div className="">Effect</div>
+                <textarea
+                    rows={40}
+                    cols={50}
+                    placeholder="Whip around like a yoyo"
+                    className="bg-dark-300 h-44"
+                    value={effectText}
+                    onChange={(e) => setEffectText(e.target.value)}
+                />
 
-            <div className="grid grid-cols-3 gap-4">
-                {displayedTraits.filter(t => (t.name == curTrait?.name)).length > 0 ? (
-                    <>
-                        <span />
-                        <Button title="Delete" className="flex flex-row" variant={'body'} onClick={handleDelete}>Delete</Button>
-                        <Button title="Update" className="flex flex-row" variant={"soul"} onClick={handleUpdate}>Update</Button>
-                    </>
-                 ): (<>
-                    <span />
-                    <span />
-                    <Button title="Create New" className="flex flex-row" variant={'nature'} onClick={handleCreateNew}>Create New</Button>
-                    </>)
-                }
+                <div className="grid grid-cols-3 gap-4">
+                    {displayedTraits.filter((t) => t.name == curTrait?.name)
+                        .length > 0 ? (
+                        <>
+                            <span />
+                            <Button
+                                title="Delete"
+                                className="flex flex-row"
+                                variant={"body"}
+                                onClick={handleDelete}
+                            >
+                                Delete
+                            </Button>
+                            <Button
+                                title="Update"
+                                className="flex flex-row"
+                                variant={"soul"}
+                                onClick={handleUpdate}
+                            >
+                                Update
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <span />
+                            <span />
+                            <Button
+                                title="Create New"
+                                className="flex flex-row"
+                                variant={"nature"}
+                                onClick={handleCreateNew}
+                            >
+                                Create New
+                            </Button>
+                        </>
+                    )}
+                </div>
+                {/* <Popup displayedContentName={popupName} displayedContent={popupData} popupIsOpen={popupIsOpen} setPopupIsOpen={(val) => {setPopupIsOpen(val);}} /> */}
             </div>
-            {/* <Popup displayedContentName={popupName} displayedContent={popupData} popupIsOpen={popupIsOpen} setPopupIsOpen={(val) => {setPopupIsOpen(val);}} /> */}
-        </div>
 
             <h1>Traits</h1>
 
             {curTrait?.name && (
                 <>
                     <div className="justify-start">
-                        <h1>
-                            Active Trait
-                        </h1>
+                        <h1>Active Trait</h1>
                         <TraitsTable
                             displayedTraits={[curTrait]}
                             moveTrait={removeFromPinnedTrait}
                             moveIsAdd={false}
                         />
                         <hr className="h-px my-4 border-0 bg-dark-600" />
-                                    
                     </div>
                 </>
             )}
@@ -417,7 +486,8 @@ export default function UpdateDBTraitsPage() {
                                     displayedTraits={displayedTraits.filter(
                                         (s) => {
                                             return s.req
-                                                ?.toString().toLowerCase()
+                                                ?.toString()
+                                                .toLowerCase()
                                                 .includes(n.toLowerCase());
                                         }
                                     )}
