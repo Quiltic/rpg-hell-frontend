13e60fd6f24bfc6b7d3e2beab8662dca51e340ac
Quiltic <jbot237@gmail.com>
2024-04-01 14:06:20 -0400

    Rickroll?

--- src/components/loginCallbackPage/LoginCallbackPage.tsx
+++ src/components/loginCallbackPage/LoginCallbackPage.tsx
@@ -3,3 +3,7 @@ import { Navigate } from "react-router-dom";
 export default function LoginCallbackPage() {
     return <Navigate to={"/"} replace />;
 }
+
+export default function RickRoll() {
+    return <Navigate to={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} replace />;
+}
