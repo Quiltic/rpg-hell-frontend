b16e9609ab8b71cef7603efcd53485fc0f798c2d
Quiltic <jbot237@gmail.com>
2024-04-01 14:17:36 -0400

    Last try

--- src/components/loginCallbackPage/RickRoll.tsx
+++ src/components/loginCallbackPage/RickRoll.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 
 export default function RickRoll() {
-    return <Navigate to={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} replace />;
+    window.location.assign('http://github.com');
+    return <></>;
 }
