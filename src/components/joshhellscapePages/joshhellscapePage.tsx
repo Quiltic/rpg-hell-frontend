b804473c69fb96c0d6ed9f0946e23afaf53b1a28
Quiltic <jbot237@gmail.com>
2024-08-19 16:44:48 -0400

    update trait backups

--- src/components/joshhellscapePages/joshhellscapePage.tsx
+++ src/components/joshhellscapePages/joshhellscapePage.tsx
@@ -1,15 +1,114 @@
 import React, { useState, useEffect } from "react";
-import DicePopup from "../ui/Popups/dicePopup";
+import { Button } from "../ui/Button/Button";
+import json from "../../assets/OfflineJsons/traits.json";
+import useApi from "../../hooks/useApi";
+import { Trait } from "../../client";
+import { sortArrayByReqs } from "../../util/sortingTools";
 
 
 
 export default function JoshhellscapePage() {
 
-    return (
-        <div className="grid auto-rows-auto grid-cols-6 gap-3 p-2">
+    const { TraitsService } = useApi();
 
-            <DicePopup/>
-            
+    const [allTraits, setAllTraits] = useState<Array<Trait>>([]);
+    // const [mainstatSkillList, setMainstatSkillList] = useState(statSkillList);
+    // const [secondstatSkillList, setSecondstatSkillList] = useState(statSkillList);
+    // const [otherList, setOtherList] = useState(otherListCore);
+    // const [diceCostList, setDiceCostList] = useState(diceCostListCore);
+    const [curID, setCurID] = useState(0);
+    const [nameText, setNameText] = useState("");
+    const [mainStat, setMainStat] = useState("MONSTER 0");
+    const [secondStat, setSecondStat] = useState("");
+    const [diceCost, setDiceCost] = useState("P");
+    const [otherDrop, setOtherDrop] = useState("");
+    const [effectText, setEffectText] = useState("");
+    const [curTrait, setCurTrait] = useState<Trait>();
+
+    async function handleUpdate() {
+        console.log(curTrait);
+        if (curTrait == undefined) {
+            return;
+        }
+        if (curTrait?.name != "") {
+            const reply = await TraitsService.updateTrait({
+                name: curTrait?.name,
+                requestBody: curTrait,
+            });
+            console.log(reply);
+        }
+    }
+
+    function addToPinnedTrait(s: Trait) {
+        setCurID(s.id);
+        setNameText(s.name);
+        setEffectText(s.effect ?? "");
+        setMainStat(s.req[0]);
+
+        if (s.req?.length > 1) {
+            // setSecondstatSkillList([,...statSkillList]);
+            setSecondStat(s.req[1]);
+        } else {
+            setSecondStat("");
+        }
+        if (s.req?.length > 2) {
+            setOtherDrop(s.req[2]);
+        } else {
+            setOtherDrop("");
+        }
+        setDiceCost(s.dice ? "#".repeat(s.dice ?? 1) : "P");
+    }
+
+    useEffect(() => {
+        setAllTraits(Object.values(json));
+    }, [TraitsService]);
+
+    // useEffect(() => {
+    //     // console.log(mainStat,secondStat,otherDrop);
+    //     const trait = {
+    //         id: curID,
+    //         name: nameText.toLowerCase(),
+    //         effect: effectText,
+    //         req: [mainStat, secondStat, otherDrop],
+    //         dice: 0,
+    //         is_passive: true,
+    //     };
+
+    //     if (diceCost != "P") {
+    //         trait.is_passive = false;
+    //         trait.dice = diceCost.split("#").length - 1;
+    //     }
+
+    //     // remove the empty stuffs
+    //     trait.req = trait.req.filter((str) => str !== "");
+
+    //     setCurTrait(trait);
+    // }, [
+    //     nameText,
+    //     diceCost,
+    //     mainStat,
+    //     secondStat,
+    //     otherDrop,
+    //     effectText,
+    //     curID,
+    // ]);
+
+
+    return (
+        <div className="p-2">
+            <Button
+                title="CheckList"
+                className="flex flex-row"
+                variant={"body"}
+                onClick={ (e) => {
+                    console.log(allTraits[60]);
+                    console.log(curTrait);
+                    setCurTrait(allTraits[60]);
+                }
+                }
+            >
+                CheckList
+            </Button>
         </div>
     );
 }
