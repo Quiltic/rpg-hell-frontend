68c35464cd0740ebd9dac384c713a0bebe4f6042
Quiltic <jbot237@gmail.com>
2024-09-15 16:29:42 -0400

    fixed a mystype on the tempsheet

--- src/components/CharacterSheet/tempsheet.ts
+++ src/components/CharacterSheet/tempsheet.ts
@@ -1,6 +1,6 @@
 
 const markdown = `
-[NAME] - LEVEL: __ [RACE]
+[NAME] - LEVEL: __ [Lineage]
 
 (Max of 4)
 Body: 0, Mind: 0, Soul: 0
@@ -8,12 +8,12 @@ Body: 0, Mind: 0, Soul: 0
 (Max of 4)
 Arcana: 0, Charm: 0, Crafting: 0, Nature: 0, Medicine: 0, Thieving: 0
 
-Strain - 0/[Body + 2*Mind + 3*Soul]
+Strain - 0/[2*Body + 3*Mind + 4*Soul]
 Armor: [0] Health: __/(4*Body + 3*Mind + 2*Soul + LEVEL), Speed [6]. 
 Locked: [], Combat Dice: __ (starts at 4, goes up every even level)
 
 **Traits:** (Trait for each level; you must meet the requirements to take the trait)
-RACE:
+Lineage:
 1: 
 1: 
 1: 
