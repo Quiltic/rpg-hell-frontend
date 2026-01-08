b9223a9d11d9b59b881a12b5543bc0a9ed6fb138
Quiltic <jbot237@gmail.com>
2026-01-07 22:25:53 -0500

    updated the traitcard

--- src/components/TraitsPages/TraitCardStuff/traitCard.tsx
+++ src/components/TraitsPages/TraitCardStuff/traitCard.tsx
@@ -4,6 +4,7 @@ import MarkdownRenderer from "../../../util/MarkdownRenderer";
 import { formatEffectString, toPillElement } from "../../../util/textFormatting";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import { cn } from "../../../styling/utilites";
 
 type Props = {
     _trait: Trait;
@@ -13,16 +14,16 @@ type Props = {
 // i fucking hate typescript, without this worthless variable the colors will simply NOT WORK
 const STUPID_COLOR_TYPESCRIPT_BS = [
 
-    "bg-gradient-to-br from-body to-body p-2",
-    "bg-gradient-to-br from-mind to-mind p-2",
-    "bg-gradient-to-br from-soul to-soul p-2",
+    "bg-gradient-to-br from-body-400 to-body-400 p-2",
+    "bg-gradient-to-br from-mind-400 to-mind-400 p-2",
+    "bg-gradient-to-br from-soul-400 to-soul-400 p-2",
     
-    "bg-gradient-to-br from-arcana to-arcana p-2",
-    "bg-gradient-to-br from-charm to-charm p-2",
-    "bg-gradient-to-br from-crafting to-crafting p-2",
-    "bg-gradient-to-br from-nature to-nature p-2",
-    "bg-gradient-to-br from-medicine to-medicine p-2",
-    "bg-gradient-to-br from-thieving to-thieving p-2",
+    "bg-gradient-to-br from-arcana-400 to-arcana-400 p-2",
+    "bg-gradient-to-br from-charm-400 to-charm-400 p-2",
+    "bg-gradient-to-br from-crafting-400 to-crafting-400 p-2",
+    "bg-gradient-to-br from-nature-400 to-nature-400 p-2",
+    "bg-gradient-to-br from-medicine-400 to-medicine-400 p-2",
+    "bg-gradient-to-br from-thieving-400 to-thieving-400 p-2",
     
 ];
 
@@ -42,43 +43,28 @@ export default function TraitCard({
     );
 
     // gives automatic gradients for trait color bar
-    let graid = `bg-gradient-to-br from-${_trait.req[0].toString().replace(/[0-9\s]/g, '') ?? ""} to-${_trait.req[0]?.toString().replace(/[0-9\s]/g, '') ?? ""} p-2`;
+    let graid = `bg-gradient-to-br from-${_trait.req[0].toString().replace(/[0-9\s]/g, '') ?? ""}-400 to-${_trait.req[0]?.toString().replace(/[0-9\s]/g, '') ?? ""}-400 p-2`;
     if (_trait.req.length > 1) {
-        graid = `bg-gradient-to-br from-${_trait.req[0].toString().replace(/[0-9\s]/g, '') ?? ""} to-${_trait.req[1].toString().replace(/[0-9\s]/g, '') ?? ""} p-2`;
+        graid = `bg-gradient-to-br from-${_trait.req[0].toString().replace(/[0-9\s]/g, '') ?? ""}-400 to-${_trait.req[1].toString().replace(/[0-9\s]/g, '') ?? ""}-400 p-2`;
     }
 
     return (
-        <div className="flex flex-col bg-dark-400 rounded-md border-solid border-2 border-body-700/20 m-4" >
-            <div className="flex flex-row items-center bg-dark rounded-md justify-between">
-                <div className="text-lg font-bold capitalize p-2">
+        <div className="flex flex-col m-4">
+            <div className="grid grid-cols-[2fr_1fr] -mb-5">
+                <div className="lg:text-lg font-bold capitalize bg-dark-600 rounded-t-lg border-solid border-2 border-body-700/20 m-4 p-2">
                     {_trait.name ?? ""}
                 </div>
-                <div className="flex flex-row items-center capitalize">
+                <div className="flex flex-col capitalize bg-dark-600 rounded-t-lg border-solid border-2 border-body-700/20 m-4 p-2">
                     {req}
                 </div>
-                
             </div>
-            
-            <div className={graid}/>
-            {/* <div
-                dangerouslySetInnerHTML={{ __html: ee }}
-                className="whitespace-pre-wrap text-left m-5 indent-5 text-sm"
-            ></div> */}
-            {/* <Markdown
-                remarkPlugins={[remarkGfm]}
-                rehypePlugins={[rehypeRaw]}
-                components={{
-                    ul: ({ node, ...props }) => <ul className="md_list" {...props} />,
-                }}
-                className="text-left"
-            >
-                {ee[0]}
-            </Markdown> */}
-
-            {ee.map((line, i) => {
-                return (
-                <>
-                <Markdown
+            <div className="flex flex-col bg-dark-400 rounded-md border-solid border-2 border-body-700/20" >
+                <div className={graid}/>
+                {/* <div
+                    dangerouslySetInnerHTML={{ __html: ee }}
+                    className="whitespace-pre-wrap text-left m-5 indent-5 text-sm"
+                ></div> */}
+                {/* <Markdown
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeRaw]}
                     components={{
@@ -86,17 +72,32 @@ export default function TraitCard({
                     }}
                     className="text-left"
                 >
-                    {line}
-                </Markdown>
-                
-                { i < ee.length-1 && 
-                    <div className="mt-2.5 w-full text-center border-b-2 border-solid border-body-700/20">
-                        {/* <div className="px-2.5 py-2 bg-dark-400 border-2 border-solid border-body-700/20">TEST</div> */}
-                    </div>
-                }
-                </>
-            );
-            })}
+                    {ee[0]}
+                </Markdown> */}
+    
+                {ee.map((line, i) => {
+                    return (
+                    <>
+                    <Markdown
+                        remarkPlugins={[remarkGfm]}
+                        rehypePlugins={[rehypeRaw]}
+                        components={{
+                            ul: ({ node, ...props }) => <ul className="md_list" {...props} />,
+                        }}
+                        className="text-left mb-2.5"
+                    >
+                        {line}
+                    </Markdown>
+                    
+                    { i < ee.length-1 && 
+                        <div className="mt-2.5 w-full text-center border-b-2 border-solid border-body-700/20">
+                            {/* <div className="px-2.5 py-2 bg-dark-400 border-2 border-solid border-body-700/20">TEST</div> */}
+                        </div>
+                    }
+                    </>
+                );
+                })}
+        </div>
         </div>
     );
 }
